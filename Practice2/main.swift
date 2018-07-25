3e40b51e0d04ceac4e58fd5079401d60f002abf0
Ryan Pliske <ryan.pliske@monsanto.com>
2018-07-25 18:07:38 -0500

    Initial Commit

---
+++ Practice2/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Practice2
+//
+//  Created by PLISKE, RYAN [AG-Contractor/1000] on 7/25/18.
+//  Copyright © 2018 Monsanto. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
